11902c9f82e6528e9a3d61f21b01a94e139cdd63
Gabriela Ferri <gabi069stefani@gmail.com>
2018-09-24 17:23:28 -0300

    feito o exercicio1 B)

--- src/Exercicios/Exercicio2.vue
+++ src/Exercicios/Exercicio2.vue
@@ -2,33 +2,33 @@
   .display-2 com v-bind
     v-container(grid-list-xl)
       v-layout(row wrap)
-        .title Exercicio 1 - Nos text-field abaixo utilize a diretiva v-bind para imprimir nomes de bandas de rock.
-        v-flex.mt-5(xs12)
+        .title Exercicio 1 - A) Nos text-field abaixo utilize a diretiva v-bind para imprimir nomes de bandas de rock.
+        v-flex.md-5(xs12)
+          span resposta:
           p(v-for= "banda in bandas") {{ banda }}
           v-text-field(:value="bandas")
-        v-flex.mt-5(xs12)
+        v-flex.md-5(xs12)
           v-text-field(:value="bandas")
-        v-flex.mt-5(xs12)
+        v-flex.md-5(xs12)
           v-text-field(:value="bandas")
         v-flex(xs12)
-        .title Exericio 1 - hardcore - Faça a mesma coisa do exercicio anterior só que utilize um array, e uma laço de repetição (v-for) no value para repetir os text-field ao menos com o nome de 3 bandas.
-        v-flex.mt-5(xs12)
+        .title B) hardcore - Faça a mesma coisa do exercicio anterior só que utilize um array, e uma laço de repetição (v-for) no value para repetir os text-field ao menos com o nome de 3 bandas.
+        v-flex.md-5(xs12)
           p(v-for= "banda in bandas") {{ banda }}
           v-text-field(value="bandas")
         .title Exericio 2 - hardcore - Montar um v-select com v-bind com um array onde ele use o array, de cores, porém imprima abaixo o número da cor.
-        v-flex.mt-5(xs12)
-          v-select(:items="cores") {{ payload.numero }}
-          .title(label="Cores"
-          item-text="titulo")
-        v-flex.mt-5(xs12)
-          .title Exericio 3 - Utilizando a diretiva v-model, altere os valores de numeroUm e numeroDois, para qualquer outro entre 100~1000, amarre os valores das variaveis numeroUm e numeroDois e faça um método que multiplique um pelo outro e imprima o resulto a frente.
-        v-flex.mt-5(xs6)
+        v-flex.md-5(xs12)
+          v-select(:items="cores")
+          .title(:items="cores")(:v-model="cores.numero")
+        v-flex.md-5(xs12)
+          .title Exericio 3 - Utilizando a diretiva v-model, altere os valores de numeroUm e numeroDois, para qualquer outro entre 100~1000, amarre os valores das variaveis numeroUm e numeroDois e faça um método que multiplique um pelo outro e imprima o resulto a frente.  {{ Math.random(1000) }}
+        v-flex.md-5(xs6)
           v-text-field(label="numeroUm")
-        v-flex.mt-5(xs6)
+        v-flex.md-5(xs6)
           v-text-field(label="numeroDois")
         v-flex(xs12)
-          .title.blue Eis seu resultado {{ numeroUm * numeroDois }}
-        v-flex.mt-5(xs12)
+          .title.teal Eis seu resultado {{ Math.random(1000) }}
+        v-flex.md-5(xs12)
           .title BONUS - Brevemente me diga o que é um computed no VUEJS. Com a mesma ideia do exercico anterior, ao invés de usar um método use uma computed
 </template>
 
@@ -36,8 +36,8 @@
 export default {
   data: () => ({
     bandas: ['velhas virgens', 'garotos podres', 'blink 182'],
-    numeroUm: 100,
-    numeroDois: 10,
+    numeroUm: 236,
+    numeroDois: 158,
     resultadoDaMultiplicacao: 0,
     cores: [
       {
@@ -57,11 +57,10 @@ export default {
         numero: 4,
       },
     ],
+    items: ['1', '2', '3', '4'],
     cores: ['branco', 'preto', 'cinza', 'azul'],
-    payload: {
-      nome:'',
-      numero:'',
-    }
+    payload: []
+
   }),
 };
 </script>
